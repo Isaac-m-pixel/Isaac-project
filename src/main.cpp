eb7746f158bc5af28a97c9ff4ecf55e787d50de4
root <root@localhost>
2024-09-26 07:33:33 +0300

    auto publish 1727325212568

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//program to print hello world-single line comment 
+/*
+program to print hello world
+Author:isaac
+Date:14/9/2024
+Reg No:
+*/
+//preprocessor directive printf()-scanf()
+#include <stdio.h>
+int main (){
+printf("isaac"/'n');
+scanf("isaac"/''n'');
+return 0;
+}
